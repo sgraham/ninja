1c16d2c95cf8fc710fc1f472294ae9718f7013a3
Nico Weber <nicolasweber@gmx.de>
2013-11-01 18:33:48 -0700

    todo list

--- bench0_sse_tok.cc
+++ bench0_sse_tok.cc
@@ -17,7 +17,7 @@ time ./ninja -C ~/src/chrome/src/out_bench/Release chrome
 using std::string;
 
 enum {
-  CHAR_HORZ_WS  = 0x01,  // ' ', '\t'.  Note, no '\0'
+  CHAR_HORZ_WS  = 0x01,  // ' '
   CHAR_LETTER   = 0x02,  // a-z,A-Z
   CHAR_NUMBER   = 0x04,  // 0-9
   CHAR_OTHERIDENT    = 0x08,  // -_.
